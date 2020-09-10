deeae54c3c6810758d372278dadbe70e524a6be7
Martin <martinboesen@hotmail.com>
2020-09-10 12:43:08 +0200

    Opsætning af repository
    Tilføjede de nødvendige mapper og filer

---
+++ js/js.js
@@ -0,0 +1,16 @@
+/*
+ * fil: js.js
+ * purpose: Script til Webdokumentaren
+ */
+console.log('file: js/js.js loaded');
+
+// A $( document ).ready() block.
+$(document).ready(function () { // kører så snart DOM er klar
+
+	console.log("jQuery 3.5.1 running. Alert level green.");
+
+	// ... din kode herfra ...
+
+	// ... din kode slut ...
+
+}); // denne line må ikke slettes
